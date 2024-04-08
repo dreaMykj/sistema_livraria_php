9379659dd2232acaa991d18886c3600f62a36a16
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-08 20:43:18 -0300

    Atividade em andamento

---
+++ insert_user.php
@@ -0,0 +1,22 @@
+<?php 
+
+$nome = $_POST['nome'];
+$setor = $_POST['setor'];
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+$confirme = $_POST['confirme'];
+
+echo "$nome";
+echo "<br>";
+echo "$setor";
+echo "<br>";
+echo "$login";
+echo "<br>";
+echo "$senha";
+echo "<br>";
+echo "$confirme";
+echo "<br>";
+
+
+
+?>
